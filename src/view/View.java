5b38fc4707fb37efd81de489da6699c3913f35c8
Thierry Belliot <thierry.belliot@student.hu.nl>
2015-12-07 13:14:30 +0100

    MVC

--- src/view/View.java
+++ src/view/View.java
@@ -1,4 +1,28 @@
 package view;
 
-public class View {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
+public class View extends Application {
+
+    public static void main(String args[]) {
+        launch(args);
+    }
+
+    public View(String args[]) {
+
+    }
+
+
+
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("PaF");
+
+        StackPane root = new StackPane();
+        primaryStage.setScene(new Scene(root, 800,600));
+        primaryStage.show();
+    }
 }
