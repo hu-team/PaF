851911f9fcf5da2059c93acdd905b2ec9ab3a152
Max van Kuik <kuikvanmax@hotmail.com>
2015-12-07 15:30:58 +0100

    Merge branch 'master' of /Users/maxvankuik/IdeaProjects/PaF Ass 2 with conflicts.

--- src/view/View.java
+++ src/view/View.java
@@ -2,21 +2,18 @@ package view;
 
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
-
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
 public class View extends Application {
-    EditorView editorview = new EditorView();
-    SelectorView selectorview = new SelectorView();
-    BorderPane root = new BorderPane();
-
     public static void main(String args[]) {
         Application.launch(args);
     }
@@ -25,19 +22,14 @@ public class View extends Application {
         MenuBar menuBar = new MenuBar();
         Menu menuFile = new Menu("File");
         MenuItem menuItemA = new MenuItem("Save As");
-        Menu menuFile1 = new Menu("Editor");
-        Menu menuFile2 = new Menu("Selector");
+        Menu menuFile1 = new Menu("View");
+        MenuItem menuItemB = new MenuItem("Editor");
+        MenuItem menuItemC = new MenuItem("Selector");
+
 
         menuFile.getItems().add(menuItemA);
-        menuBar.getMenus().addAll(menuFile,menuFile1,menuFile2);
-
-        menuFile1.setOnAction(new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent actionEvent) {
-                System.out.println("yo");
-                root.setCenter(editorview.items());
-            }
-        });
-
+        menuFile1.getItems().addAll(menuItemB, menuItemC);
+        menuBar.getMenus().addAll(menuFile,menuFile1);
         return menuBar;
     }
 
@@ -45,9 +37,8 @@ public class View extends Application {
 
         primaryStage.setTitle("PaF");
 
-
+        BorderPane root = new BorderPane();
         root.setTop(this.MenuBar());
-        root.setCenter(selectorview.items());
 
         Scene scene = new Scene(root, 800,600);
         primaryStage.setScene(scene);
